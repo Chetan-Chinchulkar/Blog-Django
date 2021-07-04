9638830c1544689a8a93803d16652794adfb1460
Chetan-Chinchulkar <chetanchinchulkar.1802@gmail.com>
2021-07-04 23:57:10 +0530

    Files updated

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 
 def home(request):
     context = {
@@ -36,5 +36,23 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
         form.instance.author =self.request.user
         return super().form_valid(form)
     
+    def test_func(self):
+        post=self.get_object()
+        
+        if self.request.user==post.author:
+            return True
+        return False
+        
+class PostDeleteView(LoginRequiredMixin ,UserPassesTestMixin ,DeleteView):
+    model=Post
+    success_url='/'
+
+    def test_func(self):
+        post=self.get_object()
+        
+        if self.request.user==post.author:
+            return True
+        return False
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
